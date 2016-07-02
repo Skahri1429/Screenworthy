713d0b5ecc1ff5e18d72064c8001963156d35fb2
Sarthak Khillon <sarthakkhillon24@gmail.com>
2016-07-02 13:34:19 -0700

    Initial Commit

---
+++ ScreenPhotos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenPhotos
+//
+//  Created by Pankaj Khillon on 7/2/16.
+//  Copyright © 2016 Sarthak Khillon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
