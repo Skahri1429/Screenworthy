3bbe6fa941d50150a5b1e058ea41a8d176eb55d7
Sarthak Khillon <sarthakkhillon24@gmail.com>
2016-07-21 02:11:03 -0700

    (Error) set up collection view, started on table view.
    Figure out how to update both views and how to display files with a collection view

--- ScreenPhotos/View Controllers/MainViewController.swift
+++ ScreenPhotos/View Controllers/MainViewController.swift
@@ -9,29 +9,31 @@
 import Foundation
 import Cocoa
 
-class MainViewController: NSViewController, NSCollectionViewDataSource {
+class MainViewController: NSViewController {
 
     @IBOutlet weak var nextButton: NSButton!
     @IBOutlet weak var cancelButton: NSButton!
-    @IBOutlet weak var eliminatedPhotoCollectionView: EliminatedPhotoCollectionView!
+    @IBOutlet weak var flaggedPhotoCollectionView: NSCollectionView!
     @IBOutlet weak var processView: ProcessView!
     
     var photosDetected: Int!
+    
+    private func configureCollectionView() {
+        let flowLayout = NSCollectionViewFlowLayout()
+        flowLayout.itemSize = NSSize(width: 160.0, height: 140.0)
+        flowLayout.sectionInset = NSEdgeInsets(top: 10.0, left: 20.0, bottom: 10.0, right: 20.0)
+        flowLayout.minimumInteritemSpacing = 20.0
+        flowLayout.minimumLineSpacing = 20.0
+        flaggedPhotoCollectionView.collectionViewLayout = flowLayout
+        
+        view.wantsLayer = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Collection View
-        eliminatedPhotoCollectionView.dataSource = self
+        configureCollectionView()
     }
-    
-    override func collectionView(collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    override func collectionView(collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
-        <#code#>
-    }
-    
     
 }
